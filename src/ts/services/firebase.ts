6f77d58fbe3e1aa0558d9eac672b14ffd23ac645
Troy DeSpain <despaintroy@gmail.com>
2021-10-02 09:48:11 -0600

    Use env variables for firebase config

--- src/ts/services/firebase.ts
+++ src/ts/services/firebase.ts
@@ -1,6 +1,17 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
-import { firebaseConfig } from 'ts/secrets'
+
+// These keys will be exposed on the client frontend, but this is safe
+// Security is enforced through Firebase security rules
+// See more: https://stackoverflow.com/questions/37482366/is-it-safe-to-expose-firebase-apikey-to-the-public/
+const firebaseConfig = {
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID,
+}
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig)
