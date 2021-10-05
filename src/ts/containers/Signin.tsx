4d422b3f194080490629cf37ddf65d4f3a8ba04c
Troy DeSpain <despaintroy@gmail.com>
2021-10-05 17:43:52 -0600

    Extract signin form to separate component

--- src/ts/containers/Signin.tsx
+++ src/ts/containers/Signin.tsx
@@ -1,65 +1,15 @@
-import React, { FormEvent, useState } from 'react'
+import React from 'react'
 
-import { Alert, Card, Form } from 'react-bootstrap'
-import LoadingButton from 'ts/components/LoadingButton'
-import { signIn } from 'ts/services/auth'
-import { getMessage } from 'ts/services/errors'
+import { Card } from 'react-bootstrap'
+import SignInForm from 'ts/components/forms/SignInForm'
 
 export default function SignIn(): React.ReactElement {
-	const [email, setEmail] = useState('')
-	const [password, setPassword] = useState('')
-	const [errorText, setErrorText] = useState<string>()
-	const [isLoading, setIsLoading] = useState(false)
-
-	const submit = (e: FormEvent<HTMLFormElement>): void => {
-		e.preventDefault()
-		setErrorText('')
-
-		if (!email || !password) setErrorText('Please enter all required fields')
-		else {
-			setIsLoading(true)
-			signIn(email, password).catch(e => {
-				setErrorText(getMessage(e))
-				setIsLoading(false)
-			})
-		}
-	}
-
 	return (
-		<>
-			<Card className='signin-card shadow mx-auto'>
-				<Card.Header>Sign In</Card.Header>
-				<Card.Body>
-					{/* <img src={process.env.PUBLIC_URL + '/logo-primary.png'} /> */}
-					<Form onSubmit={submit}>
-						<Form.Group>
-							<Form.Label>Username</Form.Label>
-							<Form.Control
-								type='text'
-								onChange={(e): void => setEmail(e.target.value)}
-							/>
-						</Form.Group>
-						<Form.Group>
-							<Form.Label>Password</Form.Label>
-							<Form.Control
-								type='password'
-								onChange={(e): void => setPassword(e.target.value)}
-							/>
-						</Form.Group>
-						{errorText && (
-							<Alert variant='secondary' className='mt-3'>
-								{errorText}
-							</Alert>
-						)}
-						<LoadingButton
-							type='submit'
-							className='mt-3 w-100'
-							buttonText='Sign In'
-							showLoading={isLoading}
-						/>
-					</Form>
-				</Card.Body>
-			</Card>
-		</>
+		<Card className='signin-card shadow mx-auto'>
+			<Card.Header>Sign In</Card.Header>
+			<Card.Body>
+				<SignInForm />
+			</Card.Body>
+		</Card>
 	)
 }
--- src/ts/services/auth.ts
+++ src/ts/services/auth.ts
@@ -3,9 +3,9 @@ import { signInWithEmailAndPassword } from 'firebase/auth'
 import { auth } from './firebase'
 
 export const signIn = (email: string, password: string): Promise<void> => {
-	return signInWithEmailAndPassword(auth, email, password).then(c => {
-		return Promise.resolve()
-	})
+	return signInWithEmailAndPassword(auth, email, password).then(() =>
+		Promise.resolve()
+	)
 }
 
 export const signOut = (): void => {
