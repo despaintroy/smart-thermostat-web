d2e2d9e97c3588755a965dcfc980ee41980116f4
Troy DeSpain <despaintroy@gmail.com>
2021-10-12 09:48:50 -0600

    Code cleanup

--- src/ts/containers/Dashboard/Dial.tsx
+++ src/ts/containers/Dashboard/Dial.tsx
@@ -1,50 +1,49 @@
 import React, { useEffect, useState } from 'react'
 
 import { SetState } from 'ts/services/models'
-import { readState, setStateDbRef, writeState } from 'ts/services/setState'
+import { getState, setStateDbRef, writeState } from 'ts/services/setState'
 
 import { onValue } from '@firebase/database'
 
 import DialCircle from './DialCircle'
 
 function Dial(): React.ReactElement {
-	const [displayTemp, setDisplayTemp] = useState<number>()
+	const [setState, setSetState] = useState<SetState>()
 	const nextChange = { temp: 65, time: '6:00pm' }
 
 	useEffect(() => {
-		readState()
-			.then(r => setDisplayTemp(r.setTemp))
+		getState()
+			.then(r => setSetState(r))
 			.catch(() => console.error('error getting setTemp'))
 	}, [])
 
 	onValue(setStateDbRef, snapshot => {
-		const data = snapshot.val() as SetState
-		if (displayTemp !== data.setTemp) setDisplayTemp(data.setTemp)
+		const s = snapshot.val() as SetState
+		if (setState && setState.setTemp !== s.setTemp) setSetState(s)
 	})
 
 	const setTemp = (temp: number): void => {
-		displayTemp &&
-			writeState({
-				setTemp: temp,
-				useSchedule: true,
-				timeStamp: new Date().getTime(),
-			})
+		writeState({
+			setTemp: temp,
+			useSchedule: true,
+			timeStamp: new Date().getTime(),
+		})
 	}
 
-	if (displayTemp === undefined) return <DialCircle> </DialCircle>
+	if (setState === undefined) return <DialCircle> </DialCircle>
 
 	return (
 		<DialCircle>
 			<div className='dial-stack'>
 				<div
 					className='adjust-button up'
-					onClick={(): void => setTemp(displayTemp + 1)}
+					onClick={(): void => setTemp(setState.setTemp + 1)}
 				>
 					&uarr;
 				</div>
 				<div>
 					<div className='text-secondary'>SET TO</div>
-					<div className='set-temp'>{displayTemp}º</div>
+					<div className='set-temp'>{setState.setTemp}º</div>
 					<div className='text-muted mt-3'>
 						Scheduled change to {nextChange.temp}º<br />
 						at {nextChange.time}
@@ -52,7 +51,7 @@ function Dial(): React.ReactElement {
 				</div>
 				<div
 					className='adjust-button down'
-					onClick={(): void => setTemp(displayTemp - 1)}
+					onClick={(): void => setTemp(setState.setTemp - 1)}
 				>
 					&darr;
 				</div>
--- src/ts/services/setState.ts
+++ src/ts/services/setState.ts
@@ -6,21 +6,17 @@ import { database } from './firebase'
 import { SetState } from './models'
 
 const dbDir = 'setState/'
-
 export const setStateDbRef = ref(database, dbDir)
 
 export const writeState = (setState: SetState): void => {
 	set(setStateDbRef, setState)
 }
 
-export const readState = (): Promise<SetState> => {
+export const getState = (): Promise<SetState> => {
 	return get(child(ref(database), dbDir))
 		.then(snapshot => {
-			if (snapshot.exists()) {
-				return Promise.resolve(snapshot.val() as SetState)
-			} else {
-				return Promise.reject()
-			}
+			if (snapshot.exists()) return Promise.resolve(snapshot.val() as SetState)
+			else return Promise.reject()
 		})
 		.catch(() => Promise.reject())
 }
